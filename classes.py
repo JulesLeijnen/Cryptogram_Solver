e5cb88ac5f6e732d9eec81358200ab4459ae45aa
BuildTools <julesleijnen14@gmail.com>
2025-02-16 15:06:18 +0100

    .

--- classes.py
+++ classes.py
@@ -1,11 +1,11 @@
 #import
-from copy import deepcopy
+import re
 #classes
 
 class DictWord:
     
     def __init__(self, word):
-        self.string = word
+        self.word = word.lower()
         self.length = len(word)
         self.containing = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
@@ -15,30 +15,48 @@ class DictWord:
 
 class LevelWord:
     
-    def __init__(self, length, knowns, allwords):
+    def __init__(self, length, knowns, allWords):
         self.length = length
         self.word = [0] * length
         for charIndex in range(0, len(knowns)):
-            self.word[charIndex] == knowns[charIndex]
-        self.possabilities = allwords
-    
+            self.word[charIndex] = knowns[charIndex]
+        self.possibilities = allWords
+        self.possibleCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        self.regexString = ""
+        
     def printWord(self):
         print(self.word)
     
     def printPossibilities(self):
-        print(self.possabilities)
+        print(self.possibilities)
     
     def possibilitiesFilterLength(self):
-        print(len(self.possabilities))
-        self.possabilities = list(filter(lambda x: x.length == self.length, self.possabilities))
-        print(len(self.possabilities))
-        for x in self.possabilities:
-            print(x.string)
+        print(len(self.possibilities))
+        self.possibilities = list(filter(lambda x: x.length == self.length, self.possibilities))
+        print(len(self.possibilities))
+        print("Shortened To Length")
 
+    # def createRegexString(self):
+    #     self.regexString = ""
+    #     for char in self.word:
+    #         print(char)
+    #         if char in self.possibleCharacters:
+    #             self.regexString = self.regexString + char
+    #         else:
+    #             self.regexString = self.regexString + "."
+    #     print(self.regexString)
+    
+    def possibilitiesFilterKnowns(self):
+        print(len(self.possibilities))
+        for letterIndex in range(0, self.length):
+            self.possibilities = list(filter(lambda x: self.word[letterIndex] == x.word[letterIndex] or (x.word[letterIndex] not in self.possibleCharacters or self.word[letterIndex] not in self.possibleCharacters), self.possibilities))
+            print(len(self.possibilities))
+        for x in self.possibilities:
+            print(x.word)
 class Level:
     
     def __init__(self, initString, allKnown):
-        self.initString = initString
+        self.initString = initString.lower()
         self.words = []
         self.allKnown = allKnown
     
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from createLevel import createLevel
 def main():
     wordsArray = loadWords()
     input("Solver for Cryptogram, press enter to go to the first step and fill in the current level\n")
-    inputstring = input("Input the current level. Place a ',' between wach letter and use the corresponding numbers for the unknowns. Locked numbers should be reported with a '*' with the number of required unlocks after it. Like this: '*2'. Ignore periods and other punctionation\n\n")
-    level = createLevel(wordsArray, inputstring)
+    inputString = input("Input the current level. Place a ',' between each letter and use the corresponding numbers for the unknowns. Locked numbers should be reported with a '*' with the number of required unlocks after it. Like this: '*2'. Ignore periods and other punctuation\n\n")
+    level = createLevel(wordsArray, inputString)
     level.words[0].possibilitiesFilterLength()
+    level.words[0].possibilitiesFilterKnowns()
     #Solve
-    #DisplayAnser(s)
+    #DisplayAnswer(s)
     return
 #Standard call
 if __name__ == "__main__":
